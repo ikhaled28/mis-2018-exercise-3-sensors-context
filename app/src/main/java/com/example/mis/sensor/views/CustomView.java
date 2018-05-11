aa4ab1aefaa83f12fb81708ab775a688bf3cd502
TJ <skeletor069@gmail.com>
2018-05-11 03:26:15 +0200

    Accelerometer View Done

--- app/src/main/java/com/example/mis/sensor/views/CustomView.java
+++ app/src/main/java/com/example/mis/sensor/views/CustomView.java
@@ -3,13 +3,26 @@ package com.example.mis.sensor.views;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Build;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
 import android.view.View;
 
+import com.example.mis.sensor.AccelerometerViewData;
+
+import java.util.ArrayList;
+
+
 public class CustomView extends View{
+
+    ArrayList<AccelerometerViewData> viewData = new ArrayList<AccelerometerViewData>();
+
+    Paint redPaint = new Paint();
+    Paint greenPaint = new Paint();
+    Paint bluePaint = new Paint();
+    Paint whitePaint = new Paint();
 
     public CustomView(Context context) {
         super(context);
@@ -34,11 +47,64 @@ public class CustomView extends View{
 
     private void init(@Nullable AttributeSet attrs) {
             // Load attributes
+
+//        viewData.add(new AccelerometerViewData(10f,18f,.3f));
+//        viewData.add(new AccelerometerViewData(.0f,20f,.3f));
+//        viewData.add(new AccelerometerViewData(7f,-20f,22f));
+//        viewData.add(new AccelerometerViewData(.9f,2f,.3f));
+
+        redPaint.setColor(Color.RED);
+        redPaint.setStrokeWidth(5);
+        greenPaint.setColor(Color.GREEN);
+        greenPaint.setStrokeWidth(5);
+        bluePaint.setColor(Color.BLUE);
+        bluePaint.setStrokeWidth(5);
+        whitePaint.setColor(Color.WHITE);
+        whitePaint.setStrokeWidth(5);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-            canvas.drawColor(Color.RED);
+
+        canvas.drawColor(Color.rgb(180,180,180));
+
+        if(viewData.size() > 1) {
+            float stepSizeX = (float) getWidth() / (float) (viewData.size() - 1);
+            if(viewData.size() < 10)
+                stepSizeX = getWidth()/10f;
+            float multiplier = 2;
+            float redY = getHeight() / 8f;
+            float greenY = 3 * getHeight() / 8f;
+            float blueY = 5 * getHeight() / 8f;
+            float whiteY = 7 * getHeight() / 8f;
+            for (int i = viewData.size() - 1; i > 0 ; i--) {
+
+                float startX = (viewData.size() - 1 - i) * stepSizeX;
+                float endX = startX + stepSizeX;
+                float startYRed = redY - multiplier * viewData.get(i).x;
+                float endYRed =  redY - multiplier * viewData.get(i - 1).x;
+                float startYGreen = greenY - multiplier * viewData.get(i).y;
+                float endYGreen =  greenY - multiplier * viewData.get(i - 1).y;
+                float startYBlue = blueY - multiplier * viewData.get(i).z;
+                float endYBlue =  blueY - multiplier * viewData.get(i - 1).z;
+                float startYWhite = whiteY - multiplier * viewData.get(i).magnitude;
+                float endYWhite =  whiteY - multiplier * viewData.get(i - 1).magnitude;
+
+                canvas.drawLine(startX,startYRed, endX, endYRed, redPaint);
+                canvas.drawLine(startX,startYGreen, endX, endYGreen, greenPaint);
+                canvas.drawLine(startX,startYBlue, endX, endYBlue, bluePaint);
+                canvas.drawLine(startX,startYWhite, endX, endYWhite, whitePaint);
+
+            }
         }
+
+
+    }
+
+    public void SetAccelerometerData(ArrayList<AccelerometerViewData> viewData){
+        this.viewData = viewData;
+        invalidate();
+    }
+
 
 }
